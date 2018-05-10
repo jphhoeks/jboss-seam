032a65740fdfc4a95ef3de90665d10e35499504c
Alberto Fernandez <infjaf@gmail.com>
2018-05-10 20:30:21 +0200

    Better exception messages

--- jboss-seam/src/main/java/org/jboss/seam/framework/Identifier.java
+++ jboss-seam/src/main/java/org/jboss/seam/framework/Identifier.java
@@ -8,51 +8,47 @@ import java.io.Serializable;
  * @author Pete Muir
  *
  */
-public abstract class Identifier<T> implements Serializable
-{
-	   
-	   private static final long serialVersionUID = 1L;
+public abstract class Identifier<T> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	private Class<?> clazz;
-	   private Object id;
-   public Identifier(Class<?> clazz, Object id)
-   {
-      if (clazz == null || id == null)
-      {
-         throw new IllegalArgumentException("Id and clazz must not be null");
-      }
-      this.clazz = clazz;
-      this.id = id;
-   }
+	private Object id;
 
+	public Identifier(Class<?> clazz, Object id) {
+		if (clazz == null && id == null) {
+			throw new IllegalArgumentException("Id and clazz must not be null");
+		}
+		if (clazz == null) {
+			throw new IllegalArgumentException("clazz must not be null, id=" + id);
+		}
+		if (id == null) {
+			throw new IllegalArgumentException("id must not be null, clazz=" + clazz.getCanonicalName());
+		}
+		this.clazz = clazz;
+		this.id = id;
+	}
 
-   public Class<?> getClazz()
-   {
-      return clazz;
-   }
-   
-   public Object getId()
-   {
-      return id;
-   }
+	public Class<?> getClazz() {
+		return clazz;
+	}
 
-   @Override
-   public boolean equals(Object other)
-   {
-      if (other instanceof Identifier)
-      {
-		 Identifier<?> that = (Identifier<?>) other; 
-         if (id == null || clazz == null)
-         {
-            throw new IllegalArgumentException("Class and Id must not be null");
-         }
-         else 
-         {
-            return this.getId().equals(that.getId()) && this.getClazz().equals(that.getClazz());
-         }
-      }
-      return false;
-   }
-   
-   public abstract Object find(T persistenceContext);
+	public Object getId() {
+		return id;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof Identifier) {
+			Identifier<?> that = (Identifier<?>) other;
+			if (this.id == null || this.clazz == null) {
+				throw new IllegalArgumentException("Class and Id must not be null");
+			} else {
+				return this.getId().equals(that.getId()) && this.getClazz().equals(that.getClazz());
+			}
+		}
+		return false;
+	}
+
+	public abstract Object find(T persistenceContext);
 
 }
