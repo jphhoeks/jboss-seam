63228aa19bb022576c19573965241eab6af55e5a
Alberto Fernandez <infjaf@gmail.com>
2020-05-21 16:28:12 +0200

    Revert excepction filter change, we need both mocks

--- jboss-seam/src/main/java/org/jboss/seam/web/ExceptionFilter.java
+++ jboss-seam/src/main/java/org/jboss/seam/web/ExceptionFilter.java
@@ -11,6 +11,7 @@ import static org.jboss.seam.annotations.Install.BUILT_IN;
 
 import java.io.IOException;
 
+import javax.faces.component.UIViewRoot;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -32,7 +33,9 @@ import org.jboss.seam.core.Manager;
 import org.jboss.seam.exception.Exceptions;
 import org.jboss.seam.log.LogProvider;
 import org.jboss.seam.log.Logging;
+import org.jboss.seam.mock.MockApplication;
 import org.jboss.seam.mock.MockExternalContext;
+import org.jboss.seam.mock.MockFacesContext;
 import org.jboss.seam.transaction.Transaction;
 import org.jboss.seam.transaction.UserTransaction;
 
@@ -78,10 +81,11 @@ public class ExceptionFilter extends AbstractFilter {
 		Lifecycle.endRequest();
 
 		//the FacesContext is gone - create a fake one for Redirect and HttpError to call
-		MockExternalContext mockExternalContext = new MockExternalContext(getServletContext(), request, response);
+		MockFacesContext facesContext = createFacesContext(request, response);
+		facesContext.setCurrent();
 
 		//Initialize the temporary context objects
-		FacesLifecycle.beginExceptionRecovery(mockExternalContext);
+		FacesLifecycle.beginExceptionRecovery(facesContext.getExternalContext());
 
 		//If there is an existing long-running conversation on
 		//the failed request, propagate it
@@ -96,19 +100,29 @@ public class ExceptionFilter extends AbstractFilter {
 		try {
 			rollbackTransactionIfNecessary();
 			Exceptions.instance().handle(e);
-		} catch (ServletException | IOException se) {
+		} catch (ServletException se) {
 			throw se;
+		} catch (IOException ioe) {
+			throw ioe;
 		} catch (Exception ehe) {
 			throw new ServletException(ehe);
 		} finally {
 			//Finally, clean up the contexts
 			try {
-				FacesLifecycle.endRequest(mockExternalContext);
+				FacesLifecycle.endRequest(facesContext.getExternalContext());
+				facesContext.release();
 				log.debug("done running exception handlers");
 			} catch (Exception ere) {
 				log.error("could not destroy contexts", ere);
 			}
 		}
+	}
+
+	private MockFacesContext createFacesContext(HttpServletRequest request, HttpServletResponse response) {
+		MockFacesContext mockFacesContext = new MockFacesContext(new MockExternalContext(getServletContext(), request, response),
+				new MockApplication());
+		mockFacesContext.setViewRoot(new UIViewRoot());
+		return mockFacesContext;
 	}
 
 	protected void rollbackTransactionIfNecessary() {
