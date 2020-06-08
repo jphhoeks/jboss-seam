af4aa267f03d06f562c62c34b0c2e59d7f56d119
Alberto Fernandez <infjaf@gmail.com>
2020-06-08 20:01:33 +0200

    fix itext example webapp
    Or at least to work my way

--- examples/itext/itext-ejb/src/main/java/org/jboss/seam/example/pdf/Lists.java
+++ examples/itext/itext-ejb/src/main/java/org/jboss/seam/example/pdf/Lists.java
@@ -1,12 +1,14 @@
 package org.jboss.seam.example.pdf;
 
-import org.jboss.seam.annotations.*;
+import org.jboss.seam.annotations.Name;
+
 
 @Name("lists")
 public class Lists{
 
     String[] numbers = {"one", "two", "three", "four", "five"};
     String[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    
     
     public String[] getNumbers() {
         return numbers;
