ee2b24f5ae0edf62baa5141283a2d926ca75729c
Alberto Fernandez <infjaf@gmail.com>
2021-03-06 12:27:31 +0100

    Override equals and hashCode

--- jboss-seam/src/main/java/org/jboss/seam/security/Role.java
+++ jboss-seam/src/main/java/org/jboss/seam/security/Role.java
@@ -1,5 +1,7 @@
 package org.jboss.seam.security;
 
+import java.util.Objects;
+
 /**
  * Represents a user role.  A conditional role is a special type of role that is assigned to a user
  * based on the contextual state of a permission check.
@@ -22,4 +24,29 @@ public class Role extends SimplePrincipal {
 	public boolean isConditional() {
 		return conditional;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(conditional);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Role other = (Role) obj;
+		return conditional == other.conditional;
+	}
+	
+	
 }
